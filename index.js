7ad36544ea611234c04fbbab2b1cf38b72a559df
Georg Sawtschuk <georg.the.moon@googlemail.com>
2017-09-22 01:18:00 +0200

    Reworked messaging

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ const USERID = CONFIG.userid;
 const KEY = CONFIG.key;
 const BOT_TOKEN = CONFIG.telegram.token;
 const CHATID = CONFIG.telegram.chatid;
+const MESS = CONFIG.telegram.send_messages;
 
 const BID = CONFIG.bid;
 const ASK = CONFIG.ask;
@@ -72,17 +73,7 @@ async function getBalance() {
     };
 }
 
-async function calculateDesiredPrices() {
-    const orderBook = await getOrderBook();
-
-    //bids - покупка - %
-    //asks - продажа + %
-    
-    let bid = parseFloat(orderBook.bids[0].real_price);
-    let ask = parseFloat(orderBook.asks[0].real_price);
-
-    log.debug("current bid = " + bid.toFixed(6));
-    log.debug("current ask = " + ask.toFixed(6));
+async function calculateDesiredPrices(bid, ask) {
     
     return {
         bid : bid / (1 + BID.percent / 100),
@@ -93,14 +84,26 @@ async function calculateDesiredPrices() {
 async function makeBid(price, balance, expires) {
 
     if(balance < MIN_ORDER) {
-        sendMessage("not enough balance " + BASE);
+        if(MESS.empty_balance) {
+            await sendMessage("not enough balance " + BASE);
+        }
         return;
     }
 
     const amount_to_sell = Math.min(balance, BID.max);
     const min_to_receive = amount_to_sell / price;
     
-    sendMessage("create bid " + ass(amount_to_sell, BASE) + " > " + ass(min_to_receive, QUOTE));
+    if(MESS.create_order) {
+        await sendMessage(`*Create bid-order:*
+\`\`\`
+${listOrders([{
+            sell_price : {
+                base : ass(amount_to_sell, BASE),
+                quote : ass(min_to_receive, QUOTE)
+            },
+            real_price : price
+        }])}\`\`\``);
+    }
 
     if(global.broadcast) {
         await golosjs.broadcast.limitOrderCreateAsync(KEY, USERID, OID++ 
@@ -116,58 +119,145 @@ function ass(a, n) {
 
 async function makeAsk(price, balance, expires) {
     
-        if(balance < MIN_ORDER) {
-            sendMessage("not enough balance " + QUOTE);
-            return;
+    if(balance < MIN_ORDER) {
+        if(MESS.empty_balance) {
+            await sendMessage("not enough balance " + QUOTE);
         }
-    
-        const amount_to_sell = Math.min(balance, BID.max);
-        const min_to_receive = amount_to_sell * price;
-        
-        sendMessage("create ask " + ass(amount_to_sell, QUOTE) + " > " + ass(min_to_receive, BASE));
-        
-        if(global.broadcast) {
-            await golosjs.broadcast.limitOrderCreateAsync(KEY, USERID, OID++
-                , ass(amount_to_sell, QUOTE), ass(min_to_receive, BASE), false, new Date(Date.now() + 60 * 60 * 1000));
-        } else {
-            log.info("no broadcast, order is not created!");
-        }
+        return;
     }
+
+    const amount_to_sell = Math.min(balance, BID.max);
+    const min_to_receive = amount_to_sell * price;
+    
+    if(MESS.create_order) {
+        await sendMessage(`*Create ask-order:*
+\`\`\`
+${listOrders([{
+                    sell_price : {
+                        base : ass(amount_to_sell, QUOTE),
+                        quote : ass(min_to_receive, BASE)
+                    },
+                    real_price : price
+                }])}\`\`\``);
+    }
+
+    if(global.broadcast) {
+        await golosjs.broadcast.limitOrderCreateAsync(KEY, USERID, OID++
+            , ass(amount_to_sell, QUOTE), ass(min_to_receive, BASE), false, new Date(Date.now() + 60 * 60 * 1000));
+    } else {
+        log.info("no broadcast, order is not created!");
+    }
+}   
     
 let prev_prices = null;
+
+async function getInfos() {
+
+    const balance = await getBalance();
+    const orderBook = await getOrderBook();
+    let bid = parseFloat(orderBook.bids[0].real_price);
+    let ask = parseFloat(orderBook.asks[0].real_price);    
+    
+    log.debug("current bid = " + bid.toFixed(6));
+    log.debug("current ask = " + ask.toFixed(6));
+
+    const prices = await calculateDesiredPrices(bid, ask);    
+
+    return {
+        balance : balance,
+        top_prices : { bid : bid, ask : ask },
+        prices : prices
+    };
+}
+
+async function sendBalance(infos, orders) {
+    let b_o_base = 0;
+    let b_o_quote = 0;
+
+    for(let order of orders) {
+        const base_amount = parseFloat(order.sell_price.base.split(" ")[0]);
+        const base_name = order.sell_price.base.split(" ")[1];
+        if(base_name == BASE) {
+            b_o_base += base_amount;
+        }
+        if(base_name == QUOTE) {
+            b_o_quote += base_amount;
+        }
+    }
+
+    const b_base = infos.balance[BASE];
+    const b_quote = infos.balance[QUOTE];
+    const b_quote_base = b_quote * infos.top_prices.ask;
+    const b_o_quote_base = b_o_quote * infos.top_prices.ask;
+    const b_sum_base = b_base + b_quote_base + b_o_base + b_o_quote_base;
+
+    await sendMessage(`*Balance:*
+${ass(b_base, BASE)} (${ass(b_o_base, BASE)})
+${ass(b_quote, QUOTE)} (${ass(b_o_quote, QUOTE)})
+*Sum:* ${ass(b_sum_base, BASE)}`);
+}
+
+function listOrders(orders) {
+    let ret = "";
+    let comma = "";
+    for(let o of orders) {
+        ret += comma + `${parseFloat(o.real_price).toFixed(6)} | ${o.sell_price.base} | ${o.sell_price.quote}`;
+        comma = "\n";
+    }
+    return ret;
+}
+
+async function sendOrders(orders) {
+    let bids = [];
+    let asks = [];
+    for(let order of orders) {
+        const base_amount = parseFloat(order.sell_price.base.split(" ")[0]);
+        const base_name = order.sell_price.base.split(" ")[1];
+        if(base_name == BASE) {
+            bids.push(order);
+        }
+        if(base_name == QUOTE) {
+            asks.push(order);
+        }
+    }
+
+    await sendMessage(`*Bids:*
+\`\`\`
+${listOrders(bids)}
+\`\`\``);
+    await sendMessage(`*Asks:*
+\`\`\`
+${listOrders(asks)}
+\`\`\``);
+
+}
 
 /**
  * The brain!
  */
 async function updateOrders() {
-    const balance = await getBalance();
-    
-    const prices = await calculateDesiredPrices();
 
-    if(prev_prices && prev_prices.bid == prices.bid && prev_prices.ask == prices.ask) {
+    let infos = await getInfos();
+    const prices = infos.prices;
+    const balance = infos.balance;
+
+    if(prev_prices && prev_prices.bid == infos.top_prices.bid && prev_prices.ask == infos.top_prices.ask) {
         //nothing changed
         return;
     }
-    prev_prices = prices;
-    log.info("balance" 
-        + "\t" + prices.bid.toFixed(6)
-        + "\t" + prices.ask.toFixed(6)
-        + "\t" + ass(balance[BASE] / prices.ask + balance[QUOTE], QUOTE) 
-        + "\t" +  ass(balance[QUOTE] * prices.bid + balance[BASE], BASE));
+    prev_prices = infos.top_prices;
 
-    const b_base = balance[BASE];
-    const b_quote = balance[QUOTE];
-    const b_quote_base = balance[QUOTE] * prices.bid + balance[BASE];
-    const b_sum_base = b_base + b_quote_base;
-    sendMessage(
-        "Bid: " + prices.bid.toFixed(6)
-        + ", Ask: " + prices.ask.toFixed(6)
-        + "\nBalance:" 
-        + "\n"+  ass(b_base, BASE) 
-        + "\n" + ass(b_quote, QUOTE) + " (" + ass(b_quote_base, BASE) + ")"
-        + "\nSum: " + ass(b_sum_base, BASE));
+    if(MESS.top_prices) {
+        await sendMessage(`*Max. bid:* ${infos.top_prices.bid.toFixed(6)} *Min. ask:* ${infos.top_prices.ask.toFixed(6)}
+`);
+    }
 
-    const orders = await getOpenOrders();
+    if(MESS.desired_prices) {
+        await sendMessage(`*Des. bid:* ${infos.prices.bid.toFixed(6)} *Des. ask:* ${infos.prices.ask.toFixed(6)}
+`);
+    }
+
+    let orders = await getOpenOrders();
 
     //log.info("desired prices", JSON.stringify(prices));
 
@@ -185,7 +275,9 @@ async function updateOrders() {
             //bid. Check if bid < desired bid price
             if(price + 0.001 < prices.bid) {
                 //cancel order and create new one
-                sendMessage("bid is less then desired, increase " + price.toFixed(6) + " / " + prices.bid.toFixed(6));
+                if(MESS.changed_desired) {
+                    await sendMessage("bid is less then desired, increase " + price.toFixed(6) + " / " + prices.bid.toFixed(6));
+                }
                 if(global.broadcast) {
                     await golosjs.broadcast.limitOrderCancelAsync(KEY, USERID, parseInt(order.orderid));
                 } else {
@@ -200,7 +292,9 @@ async function updateOrders() {
             //bid. Check if bid < desired bid price
             if(price - 0.001 > prices.ask) {
                 //cancel order and create new one
-                sendMessage("ask is greather then desired, decrease " + price + "/" + prices.ask.toFixed(6));
+                if(MESS.changed_desired) {
+                    await sendMessage("ask is greather then desired, decrease " + price + "/" + prices.ask.toFixed(6));
+                }
                 if(global.broadcast) {
                     await golosjs.broadcast.limitOrderCancelAsync(KEY, USERID, parseInt(order.orderid));
                 } else {
@@ -224,6 +318,28 @@ async function updateOrders() {
             await makeAsk(prices.ask, balance[QUOTE], expires);
         }
     }
+
+    infos = await getInfos();
+    orders = await getOpenOrders();
+
+    if(MESS.open_orders) {
+        await sendOrders(orders);
+    }
+
+    if(MESS.balance) {
+        await sendBalance(infos, orders);
+    }
+    
+    let verbose = false;
+    for(let f of Object.keys(MESS)) {
+        if(MESS[f]) {
+            verbose = true;
+            break;
+        }
+    }
+    if(verbose) {
+        await commitMessage("☰");
+    }
 }
 
 async function processBlock(bn) {
@@ -239,12 +355,14 @@ async function processBlock(bn) {
         }
         switch(op) {
             case "fill_order":
-                if(opBody.open_owner == USERID) {
-                    const base = opBody.open_pays.split(" ")[1];
-                    if(BASE == base) {
-                        await sendMessage("bought " + opBody.current_pays + " for " +  opBody.open_pays);
-                    } else {
-                        await sendMessage("sold " + opBody.current_pays + " for " +  opBody.open_pays);
+                if(MESS.filled) {
+                    if(opBody.open_owner == USERID) {
+                        const base = opBody.open_pays.split(" ")[1];
+                        if(BASE == base) {
+                            await sendMessage("bought " + opBody.current_pays + " for " +  opBody.open_pays);
+                        } else {
+                            await sendMessage("sold " + opBody.current_pays + " for " +  opBody.open_pays);
+                        }
                     }
                 }
                 break;
@@ -287,17 +405,25 @@ async function run() {
 }
 
 let bot = null;
+let message = "";
 
 async function sendMessage(msg) {
     log.info(msg);
+    message += msg + "\n";
+}
+
+
+async function commitMessage(msg) {
+    await sendMessage(msg);
     if(!BOT_TOKEN) {
         return;
     }
     try {
-        await bot.sendMessage(CHATID, msg, {parse: "Markdown"})
+        await bot.sendMessage(CHATID, message, {parse: "Markdown"})
     } catch(e) {
         log.error("unable to send message " + JSON.stringify(e));
-    }    
+    }
+    message = "";
 }
 
 async function onText(data) {
